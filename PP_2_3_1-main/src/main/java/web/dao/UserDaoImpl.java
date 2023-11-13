a6bb2168e2a3d0bb6187a18207996266a9414e32
StasenkO1337 <stasenko322@yandex.ru>
2023-11-14 03:33:56 +0500

    one more time

--- PP_2_3_1-main/src/main/java/web/dao/UserDaoImpl.java
+++ PP_2_3_1-main/src/main/java/web/dao/UserDaoImpl.java
@@ -13,31 +13,28 @@ public class UserDaoImpl implements UserDao {
 
     @PersistenceContext
     private EntityManager entityManager;
+
     @Override
     public List<User> getAllUsers() {
         return entityManager.createQuery("select u from User u", User.class).getResultList();
     }
 
-    @Transactional
     @Override
     public User getUserById(Long id) {
         return entityManager.find(User.class, id);
     }
 
-    @Transactional
     @Override
     public void addUser(User user) {
         entityManager.persist(user);
     }
 
-    @Transactional
     @Override
     public void deleteUser(Long id) {
         User user = entityManager.find(User.class, id);
         entityManager.remove(user);
     }
 
-    @Transactional
     @Override
     public void updateUser(User user) {
         entityManager.merge(user);
